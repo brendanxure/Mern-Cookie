0afdf65a17df7e89b62cb8f9eb1c967178b499a5
brendanxure <brendanxure@outlook.com>
2023-09-04 10:26:41 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 import './App.css';
+import Header from './components/Header';
 import Home from './Screens/Home';
 
 function App() {
   return (
     <div className="App">
-      <Home /> 
+      <Header />
+      <Outlet />
     </div>
   );
 }
--- frontend/src/Screens/Home.jsx
+++ frontend/src/Screens/Home.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import { Button, Card, Container } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
 
 const Home = () => {
   return (
     <div>
-        <h1>Home</h1>
+        <div className='py-5'>
+          <Container className='d-flex justify-content-center'>
+            <Card className='p-5 d-flex flex-column align-items-center bg-light w-75 hero-card'>
+              <h1 className='text-center mb-4'>MERN Authentication</h1>
+              <p className='text-center mb-4'>
+                This is a boilerplate for MERN authentication that stores a JWT in an HTTP-Only cookie. It also uses Redux ToolKit and the React bootstrap library
+              </p>
+              <div className='d-flex'>
+                <LinkContainer to='/login'>
+                <Button variant='primary' className='me-3'>
+                  Sign In
+                </Button>
+                </LinkContainer>
+                <LinkContainer to='/register'>
+                <Button variant='secondary'>
+                  Register
+                </Button>
+                </LinkContainer>
+              </div>
+            </Card>
+          </Container>
+        </div>
     </div>
   )
 }
